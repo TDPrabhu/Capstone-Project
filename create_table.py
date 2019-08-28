281bf56d554c157ad240fc768016f94e3c45ef1b
Prabhu Thambidurai <tdprabhu87@gmail.com>
2019-08-28 12:41:52 +0530

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -16,7 +16,6 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
     print('Connecting to redshift')
-    #con= psycopg2.connect(dbname= 'immigration', host='redshift-cluster-1.ckxqlugu6vhf.us-east-1.redshift.amazonaws.com',       port= 5439, user= 'redshiftuser', password= 'Qazwsx!234!')
     conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     print('Connected to redshift')
     cur = conn.cursor()
